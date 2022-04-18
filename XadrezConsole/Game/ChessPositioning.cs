aed629ac1012cf371bf003fe21dd7aadaa8c2684
Nilton <ni.flavio@hotmail.com>
2022-04-17 21:59:44 -0300

    Accordingly to Real chess positioning

---
+++ XadrezConsole/Game/ChessPositioning.cs
@@ -0,0 +1,26 @@
+﻿using Board;
+
+namespace Game
+{
+    internal class ChessPositioning
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPositioning(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
