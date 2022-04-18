f4f186d94a2dab97b9652ab4453e102a21e039cc
Nilton <ni.flavio@hotmail.com>
2022-04-17 22:09:42 -0300

    Colored Pieces

--- XadrezConsole/Board/Screen.cs
+++ XadrezConsole/Board/Screen.cs
@@ -6,6 +6,7 @@
         {
             for (int i = 0; i < board.Lines; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.Piece(i, j) == null)
@@ -14,10 +15,27 @@
                     }
                     else
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -10,6 +10,7 @@ namespace XadrezConsole
             Board.Board board = new Board.Board(8,8);
             board.PlacePiece(new King(Color.Black, board), new Position(2,3));
             board.PlacePiece(new Tower(Color.Black, board), new Position(2,4));
+            board.PlacePiece(new Tower(Color.White, board), new Position(2, 1));
             Screen.Print(board);
         }
     }
